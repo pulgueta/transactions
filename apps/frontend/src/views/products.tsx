6b287265fb839491a5eceb2f2544f96ff4f0cfe4
pulgueta <retardix456@gmail.com>
2024-09-12 15:23:47 -0500

    feat: init frontend application with shadcn

---
+++ apps/frontend/src/views/products.tsx
@@ -0,0 +1,7 @@
+export const Products = () => {
+  return (
+    <main className="min-h-dvh p-4">
+      <h1>Products view</h1>
+    </main>
+  );
+};
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import biome from "vite-plugin-biome";
@@ -23,6 +25,11 @@ export default defineConfig(({ mode }) => {
           target: API,
           changeOrigin: true,
         },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
       },
     },
   };
